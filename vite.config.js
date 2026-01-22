921794241627bc727f8b8b7aafbccfc84f9a19d8
xarixme <xarixme@gmail.com>
2026-01-23 02:18:06 +1100

    chore: add vite.config.js for multi-page build

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                docs: resolve(__dirname, 'docs.html'),
+                details: resolve(__dirname, 'details.html'),
+            },
+        },
+    },
+});
